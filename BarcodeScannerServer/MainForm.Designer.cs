6e0fb8ab587550765f38555353857ac1282ce404
hexhoc <hexhoc@gmail.com>
2025-10-30 17:38:23 +0300

    Initial commit

---
+++ BarcodeScannerServer/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BarcodeScannerServer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            buttonStart = new Button();
+            notifyIconTray = new NotifyIcon(components);
+            toolStripButton1 = new ToolStripButton();
+            portTextBox = new TextBox();
+            portLabel = new Label();
+            buttonStop = new Button();
+            toolStripButton2 = new ToolStripButton();
+            toolStripAboutApp = new ToolStrip();
+            richTextBoxLog = new RichTextBox();
+            buttonClearLogs = new Button();
+            toolStripAboutApp.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(599, 401);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(75, 23);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Пуск";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += ButtonStart_Click;
+            // 
+            // notifyIconTray
+            // 
+            notifyIconTray.Icon = (Icon)resources.GetObject("notifyIconTray.Icon");
+            notifyIconTray.Text = "notifyIconTray";
+            notifyIconTray.Visible = true;
+            notifyIconTray.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 23);
+            // 
+            // portTextBox
+            // 
+            portTextBox.Location = new Point(12, 401);
+            portTextBox.Name = "portTextBox";
+            portTextBox.Size = new Size(100, 23);
+            portTextBox.TabIndex = 1;
+            portTextBox.Text = "52000";
+            // 
+            // portLabel
+            // 
+            portLabel.AutoSize = true;
+            portLabel.Location = new Point(13, 380);
+            portLabel.Name = "portLabel";
+            portLabel.Size = new Size(35, 15);
+            portLabel.TabIndex = 2;
+            portLabel.Text = "Порт";
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(693, 401);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(75, 23);
+            buttonStop.TabIndex = 3;
+            buttonStop.Text = "Стоп";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += ButtonStop_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(86, 22);
+            toolStripButton2.Text = "О программе";
+            // 
+            // toolStripAboutApp
+            // 
+            toolStripAboutApp.Items.AddRange(new ToolStripItem[] { toolStripButton2 });
+            toolStripAboutApp.Location = new Point(0, 0);
+            toolStripAboutApp.Name = "toolStripAboutApp";
+            toolStripAboutApp.Size = new Size(800, 25);
+            toolStripAboutApp.TabIndex = 0;
+            // 
+            // richTextBoxLog
+            // 
+            richTextBoxLog.Location = new Point(8, 31);
+            richTextBoxLog.Name = "richTextBoxLog";
+            richTextBoxLog.Size = new Size(780, 310);
+            richTextBoxLog.TabIndex = 4;
+            richTextBoxLog.Text = "";
+            // 
+            // buttonClearLogs
+            // 
+            buttonClearLogs.Location = new Point(12, 347);
+            buttonClearLogs.Name = "buttonClearLogs";
+            buttonClearLogs.Size = new Size(75, 23);
+            buttonClearLogs.TabIndex = 5;
+            buttonClearLogs.Text = "Очистить логи";
+            buttonClearLogs.UseVisualStyleBackColor = true;
+            buttonClearLogs.Click += ButtonClearLogs_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonClearLogs);
+            Controls.Add(richTextBoxLog);
+            Controls.Add(buttonStop);
+            Controls.Add(portLabel);
+            Controls.Add(portTextBox);
+            Controls.Add(toolStripAboutApp);
+            Controls.Add(buttonStart);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Сервер работы с драйвером сканера штрихкода";
+            FormClosing += MainForm_FormClosing;
+            Load += MainForm_Load;
+            Resize += MainForm_Resize;
+            toolStripAboutApp.ResumeLayout(false);
+            toolStripAboutApp.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonStart;
+        private NotifyIcon notifyIconTray;
+        private ToolStripButton toolStripButton1;
+        private TextBox portTextBox;
+        private Label portLabel;
+        private Button buttonStop;
+        private ToolStripButton toolStripButton2;
+        private ToolStrip toolStripAboutApp;
+        private RichTextBox richTextBoxLog;
+        private Button buttonClearLogs;
+    }
+}
---
+++ BarcodeScannerServer/MainForm.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace BarcodeScannerServer
+{
+    public partial class MainForm : Form
+    {
+        private HttpListener httpListener;
+        private CancellationTokenSource cancellationTokenSource;
+        private bool isRunning = false;
+
+        // Colors for different log types
+        private readonly Color ColorNormal = Color.Black;
+        private readonly Color ColorError = Color.Red;
+        private readonly Color ColorSuccess = Color.Green;
+        private readonly Color ColorWarning = Color.Orange;
+        private readonly Color ColorInfo = Color.Blue;
+        private readonly Color ColorReceived = Color.Purple;
+        private readonly Color ColorSent = Color.DarkGreen;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Update UI to reflect initial state
+            UpdateUIState();
+
+            // Add initial log message
+            LogMessage("Application started. Click 'Start' to begin WebSocket server.", ColorInfo);
+        }
+
+        private async void ButtonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Start the WebSocket server
+                await StartWebSocketServer();
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Failed to start server: {ex.Message}", ColorError);
+                MessageBox.Show($"Failed to start server: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            StopWebSocketServer();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            // we check our window, and if it has been minimized, we make an event       
+            if (WindowState == FormWindowState.Minimized)
+            {
+                // hiding our window from the panel
+                this.ShowInTaskbar = false;
+                // making our tray icon active
+                notifyIconTray.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            notifyIconTray.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private Task StartWebSocketServer()
+        {
+            if (isRunning)
+            {
+                return Task.CompletedTask;
+            }
+
+            string serverUrl = $"http://localhost:{portTextBox.Text}/";
+
+            // Configure the server to listen on localhost port 
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add(serverUrl);
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                httpListener.Start();
+                isRunning = true;
+                UpdateUIState();
+
+                // Log server start
+                LogMessage($"WebSocket server started on ws://localhost:{portTextBox.Text}/", ColorSuccess);
+                LogMessage("Server is listening for incoming connections...", ColorInfo);
+
+                // Start listening for connections in background
+                _ = Task.Run(async () => await ListenForConnections());
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"ERROR starting server: {ex.Message}", ColorError);
+                isRunning = false;
+                UpdateUIState();
+                throw;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ListenForConnections()
+        {
+            var token = cancellationTokenSource.Token;
+
+            while (!token.IsCancellationRequested && httpListener.IsListening)
+            {
+                try
+                {
+                    // Wait for incoming connection
+                    var context = await httpListener.GetContextAsync();
+
+                    if (context.Request.IsWebSocketRequest)
+                    {
+                        // Accept the WebSocket connection
+                        var webSocketContext = await context.AcceptWebSocketAsync(null);
+                        var webSocket = webSocketContext.WebSocket;
+
+                        LogMessage($"Client connected from {context.Request.RemoteEndPoint}", ColorSuccess);
+
+                        // Handle the WebSocket connection in background
+                        _ = Task.Run(async () => await HandleWebSocketConnection(webSocket));
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                        context.Response.Close();
+                        LogMessage($"Rejected non-WebSocket request from {context.Request.RemoteEndPoint}", ColorWarning);
+                    }
+                }
+                catch (HttpListenerException)
+                {
+                    // This is expected when stopping the listener
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    LogMessage($"Error handling connection: {ex.Message}", ColorError);
+                }
+            }
+        }
+
+
+        private async Task HandleWebSocketConnection(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var token = cancellationTokenSource.Token;
+
+            try
+            {
+                while (webSocket.State == WebSocketState.Open && !token.IsCancellationRequested)
+                {
+                    // Wait for message from client
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        // Process received message
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        LogMessage($"Received: {message}", ColorReceived);
+
+                        // Echo the message back to client
+                        var response = $"Echo: {message}";
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        await webSocket.SendAsync(
+                            new ArraySegment<byte>(responseBytes),
+                            WebSocketMessageType.Text,
+                            true,
+                            token);
+
+                        LogMessage($"Sent: {response}", ColorSent);
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                            "Closed by client", token);
+                        LogMessage("Client disconnected gracefully", ColorInfo);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Expected when server is stopping
+                LogMessage("Client connection terminated (server shutdown)", ColorWarning);
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"WebSocket error: {ex.Message}", ColorError);
+            }
+            finally
+            {
+                webSocket?.Dispose();
+            }
+        }
+
+        private void StopWebSocketServer()
+        {
+            if (!isRunning) return;
+
+            try
+            {
+                LogMessage("Stopping WebSocket server...", ColorWarning);
+                cancellationTokenSource?.Cancel();
+                httpListener?.Stop();
+                isRunning = false;
+                UpdateUIState();
+                LogMessage("WebSocket server stopped successfully", ColorSuccess);
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"Error stopping server: {ex.Message}", ColorError);
+            }
+        }
+
+        // Updating the form and its elements
+        private void UpdateUIState()
+        {
+            // Update UI elements based on server state
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateUIState));
+                return;
+            }
+
+            // Setting the accessibility of the buttons
+            buttonStart.Enabled = !isRunning;
+            buttonStop.Enabled = isRunning;
+
+            // Updating the form title
+            this.Text = $"Barcode Scanner Server - {(isRunning ? "RUNNING" : "STOPPED")}";
+        }
+
+        // Logging the event in the RichTextBox on the form
+        private void LogMessage(string message, Color color)
+        {
+            // Add timestamp and log to RichTextBox
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string, Color>(LogMessage), message, color);
+                return;
+            }
+
+            int maxLines = 10000; // Keep last 10,000 lines
+            string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
+            string logEntry = $"[{timestamp}] {message}\n";
+
+            // Check if we need to trim old lines
+            if (richTextBoxLog.Lines.Length >= maxLines)
+            {
+                TrimOldLines(maxLines);
+            }
+
+            // Append to RichTextBox with color and auto-scroll to bottom
+            richTextBoxLog.SelectionStart = richTextBoxLog.TextLength;
+            richTextBoxLog.SelectionLength = 0;
+            richTextBoxLog.SelectionColor = color;
+            richTextBoxLog.AppendText(logEntry);
+            richTextBoxLog.SelectionColor = richTextBoxLog.ForeColor; // Reset to default color
+
+            // Auto-scroll to the bottom
+            richTextBoxLog.ScrollToCaret();
+        }
+
+        // Truncating the logs. RichTextBox has a limit of ~2.1 billion characters.
+        private void TrimOldLines(int maxLines)
+        {
+            try
+            {
+                // Keep only the most recent lines
+                var lines = richTextBoxLog.Lines;
+                if (lines.Length > maxLines)
+                {
+                    int removeCount = lines.Length - maxLines;
+                    int removeLength = 0;
+
+                    // Calculate length of lines to remove
+                    for (int i = 0; i < removeCount; i++)
+                    {
+                        removeLength += lines[i].Length + 1; // +1 for newline
+                    }
+
+                    // Remove old lines
+                    richTextBoxLog.Select(0, removeLength);
+                    richTextBoxLog.SelectedText = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Fallback: clear and add message
+                richTextBoxLog.Clear();
+                richTextBoxLog.AppendText($"Log cleared due to size management. Previous logs trimmed.\n");
+            }
+        }
+
+        // The log cleanup button
+        private void ButtonClearLogs_Click(object sender, EventArgs e)
+        {
+            richTextBoxLog.Clear();
+        }
+
+        // Actions when closing the form
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopWebSocketServer(); // Останавливаем сервер
+            cancellationTokenSource?.Dispose();
+            httpListener?.Close();
+        }
+    }
+
+}
